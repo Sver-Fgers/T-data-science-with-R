10b9c6668bb00fff04222f6bcaa8967fe03ca8e2
odogwuirene <odogwuifeomairene@gmail.com>
2026-01-16 20:15:08 +0100

    16-01-2026

---
+++ 16-01-2026-live-session.R
@@ -0,0 +1,18 @@
+print("excited to start r")
+print("10 days of christmas")
+
+
+View(women)
+
+
+
+
+
+
+
+ebola_data <- read.csv("https://tinyurl.com/ebola-data-sample")
+
+pacman::p_load(highcharter)
+
+
+highcharter::hchart(women$weight)
